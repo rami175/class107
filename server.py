32bcbb173ed4e136ecf0b5ad972bcd88f8b5afa0
Rami <you@example.com>
2024-03-11 15:19:14 -0700

    first

---
+++ server.py
@@ -0,0 +1,60 @@
+from flask import Flask
+from flask import request
+import json
+products = []
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "Hello from flask"
+
+@app.get("/testing")
+def test():
+    return "Hello from another page"
+
+@app.get("/about")
+def about():
+    me = {"name":"Rami", "age":43}
+    return json.dumps(me)
+
+@app.get("/version")
+def version():
+    version = {"name": "mouse","version":"2","build":123456}
+    return json.dumps(version)
+
+@app.get("/blog")
+def blog():
+    return "Blog page"
+
+#read and write into the server
+
+@app.get("/api/products")
+def get_products():
+    return json.dumps(products)
+
+@app.post("/api/products")
+def save_products():
+    #should save a new product
+    product = request.get_json()
+    print(product)
+    #mock the save
+    products.append(product)
+    return json.dumps(product)
+
+@app.post("/post_test")
+def save_test():
+    test1 = {
+    "title":"test1",
+    "price":123,
+    "category":"test"
+    }
+    return json.dumps(test1)
+
+
+
+app.run(debug=True)#apply the changes on the code live 
+
+#API/ Endpoint
+
+#Transform JSON/ convert JSON into object again
---
+++ test.py
@@ -0,0 +1,43 @@
+# print("Hello world!")
+
+# name = "Rami"
+# last_name = "Mahmoud"
+# total = 12.99
+# age = 100
+# found =True
+# if age < 100:
+#     print("you are not old")
+#     print("this is example")
+# elif age == 100:
+#     print("congrats you'r a century")
+# else:
+#     print("sorry, you are old")
+    
+# #function
+# def say_hello():
+#     print("Hello there")
+    
+# def say_goodbye(name):
+#     print("Goodbye "+ name)
+# #call the fnction
+# say_hello()
+# say_goodbye("John")
+# #concatenating
+# print("My name is " + name + " and my age is " + str(age))
+
+#array
+#list
+# color = ["white","red","black","blue"]
+# print(color)
+# #add
+# color.append("pink")
+# print(color)
+# #traveling the list
+# for x in color:
+#     print(x)
+me={
+    "first_name":"Rami",
+    "last_name":"Mahmoud",
+    "age":100
+}
+print(me["age"])
